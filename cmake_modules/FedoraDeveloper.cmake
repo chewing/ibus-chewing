27b022e2a5b9cd25607fbd8d151c2055ad716ec8
Ding-Yi Chen <dingyichentw@yahoo.com>
2010-08-04 16:53:57 +1000

    Use GIT infrastructure (W.I.P)

--- cmake_modules/FedoraDeveloper.cmake
+++ cmake_modules/FedoraDeveloper.cmake
@@ -6,7 +6,7 @@
 #===================================================================
 # Variables:
 # KOJI_DEPENDS: Files that koji build targets depend on.
-# KOJI_CVS_PATH: The path for KOJI build.
+# FEDPKG_DIR: The path for KOJI build.
 #         Default: ./
 #
 # RPM_RELEASE_SUMMARY: Summary of latest change, used in CVS message and Bodhi
@@ -31,72 +31,105 @@
 #
 
 INCLUDE(RPM)
-SET(RAWHIDE_VER 14)
-SET(FEDORA_DIST_TAGS F-12 F-13)
+SET(RAWHIDE_VER f14)
+SET(FEDORA_DIST_TAGS f12 f13)
 
-IF(NOT DEFINED KOJI_CVS_PATH)
-    SET(KOJI_CVS_PATH "./")
-ELSEIF (KOJI_CVS_PATH STREQUAL "")
-    SET(KOJI_CVS_PATH "./")
-ENDIF(NOT DEFINED KOJI_CVS_PATH)
+IF(NOT DEFINED FEDPKG_DIR)
+    SET(FEDPKG_DIR "./FedPkg")
+ELSEIF (FEDPKG_DIR STREQUAL "")
+    SET(FEDPKG_DIR "./FedPkg")
+ENDIF(NOT DEFINED FEDPKG_DIR)
 
 #====================================================================
 # Koji (Fedora build system)
 #
 
-# Koji submit
-SET (KOJI_SUBMISSION_CMD "")
-IF (DEFINED RPM_RELEASE_SUMMARY)
-    SET (KOJI_SUBMISSION_CMD
-	"${KOJI_CVS_PATH}/${PROJECT_NAME}/common/cvs-import.sh -m \"${RPM_RELEASE_SUMMARY}\" ${SRPM_FILE}"
+ADD_CUSTOM_COMMAND(${FEDPKG_DIR}
+    COMMAND mkdir -p ${FEDPKG_DIR}
     )
 
-    FOREACH(_dist_tag ${FEDORA_DIST_TAGS})
-	SET (KOJI_SUBMISSION_CMD
-	    "${KOJI_SUBMISSION_CMD}\;"
-	    "${KOJI_CVS_PATH}/${PROJECT_NAME}/common/cvs-import.sh -b ${_dist_tag} -m \"${RPM_RELEASE_SUMMARY}\" ${SRPM_FILE}"
-	)
-    ENDFOREACH(_dist_tag)
-ELSE(DEFINED RPM_RELEASE_SUMMARY)
-    SET (KOJI_SUBMISSION_CMD
-	"${KOJI_CVS_PATH}/${PROJECT_NAME}/common/cvs-import.sh  ${SRPM_FILE}"
+ADD_CUSTOM_COMMAND(${FEDPKG_DIR}/${PROJECT_NAME}
+    COMMAND fedpkg clone ${PROJECT_NAME}
+    WORKING_DIRECTORY ${FEDPKG_DIR}
     )
-    FOREACH(_dist_tag ${FEDORA_DIST_TAGS})
-	SET (KOJI_SUBMISSION_CMD
-	    "${KOJI_SUBMISSION_CMD}\;"
-	    "${KOJI_CVS_PATH}/${PROJECT_NAME}/common/cvs-import.sh -b ${_dist_tag}  ${SRPM_FILE}"
+
+## Koji scratch build
+SET (KOJI_SCRATCH_BUILD_CMD
+    "koji build --scratch dist-${RAWHIDE_VER} ${SRPM_FILE}"
+    )
+FOREACH(_dist_tag ${FEDORA_DIST_TAGS})
+    SET (KOJI_SCRATCH_BUILD_CMD
+	"${KOJI_SCRATCH_BUILD_CMD}\;"
+	"koji build --scratch dist-${_dist_tag} ${SRPM_FILE}\;"
 	)
-    ENDFOREACH(_dist_tag)
+ENDFOREACH(_dist_tag)
+
+
+
+## Koji submit
+#import
+SET (KOJI_SUBMISSION_CMD
+    "git checkout master\;"
+    "fedpkg import  ${SRPM_FILE}\;"
+)
+FOREACH(_dist_tag ${FEDORA_DIST_TAGS})
+    SET (KOJI_SUBMISSION_CMD
+	"${KOJI_SUBMISSION_CMD}\;"
+	"fedpkg import -b ${_dist_tag}  ${SRPM_FILE}\;"
+    )
+ENDFOREACH(_dist_tag)
+
+#commit
+IF (DEFINED RPM_RELEASE_SUMMARY)
+    SET (COMMIT_MSG  "-m \"${RPM_RELEASE_SUMMARY}\"")
+ELSE(DEFINED RPM_RELEASE_SUMMARY)
+    SET (COMMIT_MSG  "")
 ENDIF(DEFINED RPM_RELEASE_SUMMARY)
 
+SET (KOJI_SUBMISSION_CMD
+    "${KOJI_SUBMISSION_CMD}\;"
+    "git checkout master\;"
+    "fedpkg commit -p ${COMMIT_MSG}\;"
+)
+FOREACH(_dist_tag ${FEDORA_DIST_TAGS})
+    SET (KOJI_SUBMISSION_CMD
+	"${KOJI_SUBMISSION_CMD}\;"
+	"fedpkg switch-branch ${_dist_tag}\;"
+	"fedpkg commit -p ${COMMIT_MSG}\;"
+    )
+ENDFOREACH(_dist_tag)
+
+## Koji build
 SET (KOJI_BUILD_CMD
-    "cd ${KOJI_CVS_PATH}/${PROJECT_NAME} && cvs up && cd devel && make build && cd .."
-    )
+    "git checkout master\;"
+    "fedpkg build\;"
+)
+FOREACH(_dist_tag ${FEDORA_DIST_TAGS})
+    SET (KOJI_BUILD_CMD
+	"${KOJI_BUILD_CMD}\;"
+	"fedpkg switch-branch ${_dist_tag}\;"
+	"fedpkg build\;"
+	)
+ENDFOREACH(_dist_tag)
 
-SET (KOJI_SCRATCH_BUILD_CMD
-    "koji build --scratch dist-f${RAWHIDE_VER} ${SRPM_FILE}"
-    )
 
-
+## Bodhi
 FOREACH(_dist_tag ${FEDORA_DIST_TAGS})
     SET (KOJI_BUILD_CMD
 	"${KOJI_BUILD_CMD} && cd ${_dist_tag} && make build && cd .."
 	)
-    STRING(REGEX MATCH "F-([0-9]*)"
+    STRING(REGEX MATCH "f([0-9]*)"
     	_is_fedora_tag ${_dist_tag})
-    STRING(REGEX MATCH "EL-([0-9]*)"
+    STRING(REGEX MATCH "el([0-9]*)"
 	_is_epel_tag ${_dist_tag})
     IF (_is_fedora_tag)
-	STRING(REGEX REPLACE "F-([0-9]*)" "\\1"
+	STRING(REGEX REPLACE "f([0-9]*)" "\\1"
 	    _curr_tag ${_dist_tag})
-	SET (KOJI_SCRATCH_BUILD_CMD
-	    "${KOJI_SCRATCH_BUILD_CMD} && koji build --scratch dist-f${_curr_tag} ${SRPM_FILE}"
-	    )
 	IF(NOT DEFINED BODHI_DIST_TAGS)
 	    SET(_bodhi_dist_tags ${_bodhi_dist_tags} "fc${_curr_tag}")
 	ENDIF(NOT DEFINED BODHI_DIST_TAGS)
     ELSEIF (_is_epel_tag)
-	STRING(REGEX REPLACE "EL-([0-9]*)" "\\1"
+	STRING(REGEX REPLACE "el([0-9]*)" "\\1"
 	    _curr_tag ${_dist_tag})
 	SET (KOJI_SCRATCH_BUILD_CMD
 	    "${KOJI_SCRATCH_BUILD_CMD} && koji build --scratch dist-${_curr_tag}E-epel-testing-candidate ${SRPM_FILE}"
@@ -107,10 +140,11 @@ FOREACH(_dist_tag ${FEDORA_DIST_TAGS})
     ENDIF(_is_fedora_tag)
 ENDFOREACH(_dist_tag)
 
+
 #MESSAGE(KOJI_SCRATCH_BUILD_CMD=${KOJI_SCRATCH_BUILD_CMD})
 ADD_CUSTOM_TARGET(koji_scratch_build
     COMMAND eval ${KOJI_SCRATCH_BUILD_CMD}
-    DEPENDS ${KOJI_DEPENDS} ${SRPM_FILE} ${KOJI_CVS_PATH}
+    DEPENDS ${KOJI_DEPENDS} ${SRPM_FILE} ${FEDPKG_DIR}
     COMMENT "Start Koji scratch build"
     VERBATIM
     )
@@ -118,8 +152,8 @@ ADD_CUSTOM_TARGET(koji_scratch_build
 #MESSAGE(KOJI_SUBMISSION_CMD=${KOJI_SUBMISSION_CMD})
 ADD_CUSTOM_TARGET(koji_submit
     COMMAND eval ${KOJI_SUBMISSION_CMD}
-    COMMAND cd ${KOJI_CVS_PATH}/${PROJECT_NAME} &&  cvs up
-    DEPENDS ${KOJI_DEPENDS}  ${SRPM_FILE} ${KOJI_CVS_PATH}
+    COMMAND cd ${FEDPKG_DIR}/${PROJECT_NAME} &&  cvs up
+    DEPENDS ${KOJI_DEPENDS}  ${SRPM_FILE} ${FEDPKG_DIR}
     COMMENT "Submitting to Koji"
     VERBATIM
     )
@@ -130,6 +164,7 @@ ADD_CUSTOM_TARGET(koji_build
     COMMENT "Building on Koji"
     VERBATIM
     )
+
 
 #====================================================================
 # Bodhi (Fedora update system)
