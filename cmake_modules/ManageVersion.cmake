f8febfde79cfb055451eb6875b1eae4294296d29
Ding-Yi Chen <dingyichen@gmail.com>
2010-08-22 09:48:39 +1000

    W.I.P

--- cmake_modules/ManageVersion.cmake
+++ cmake_modules/ManageVersion.cmake
@@ -12,21 +12,18 @@ IF(NOT DEFINED _MANAGE_VERSION_CMAKE_)
     SET(_MANAGE_VERSION_CMAKE_ "DEFINED")
     INCLUDE(ManageVariable)
     MACRO(LOAD_RELEASE_FILE filename)
-	SETTING_FILE_GET_ATTRIBUTE(${_prj_ver} "PRJ_VER" ${fliename})
+	SETTING_FILE_GET_ATTRIBUTE(_prj_ver "PRJ_VER" "${filename}")
 	SET(PRJ_VER ${_prj_ver})
-	#IF(_prj_ver)
-	#    SET(PRJ_VER_PATTERN ${_prj_ver} CACHE INTERNAL "PRJ_VER")
-	#ENDIF(_prj_ver)
-	# MESSAGE("### -1 PRJ_VER=${PRJ_VER}")
-    ENDMACRO(LOAD_RELEASE_FILE filename)
 
-    #    SET(_version_check_ver grep -e 'PRJ_VER=' ${RELEASE_FILE} |  tr -d '\\r\\n' | sed -e s/PRJ_VER=//)
-    #ADD_CUSTOM_TARGET(version_check
-    #	COMMAND ${CMAKE_COMMAND} -E echo "PRJ_VER=${PRJ_VER}"
-#   	COMMAND ${CMAKE_COMMAND} -E echo "Release file="`eval \"${_version_check_ver}\"`
-#	COMMAND test \"`${_version_check_ver}`\" = \"\" -o \"`${_version_check_ver}`\" = "${PRJ_VER}"
-#	|| echo Inconsistent version detected. Fixing.. && ${CMAKE_COMMAND} ${CMAKE_SOURCE_DIR}
-#	)
+	# PRJ_VER won' update until the execution of cmake .
+	SET(_version_check_cmd grep -e 'PRJ_VER=' ${RELEASE_FILE} |  tr -d '\\r\\n' | sed -e s/PRJ_VER=//)
+	ADD_CUSTOM_TARGET(version_check
+	    COMMAND ${CMAKE_COMMAND} -E echo "PRJ_VER=${PRJ_VER}"
+	    COMMAND ${CMAKE_COMMAND} -E echo "Release file="`eval \"${_version_check_cmd}\"`
+	    COMMAND test \"`${_version_check_cmd}`\" = \"\" -o \"`${_version_check_cmd}`\" = "${PRJ_VER}"
+	    || echo Inconsistent version detected. Fixing.. && ${CMAKE_COMMAND} ${CMAKE_SOURCE_DIR}
+	    )
+    ENDMACRO(LOAD_RELEASE_FILE filename)
 
 ENDIF(NOT DEFINED _MANAGE_VERSION_CMAKE_)
 
