33f9f19b19a0694457c4ee89eac9eb3f6a9030b1
Ding-Yi Chen <dingyichen@gmail.com>
2010-09-03 06:23:44 +1000

    Modify UseDoxygen.cmake

--- cmake_modules/UseDoxygen.cmake
+++ cmake_modules/UseDoxygen.cmake
@@ -1,11 +1,20 @@
 # - Generate Doxyfile
-
+# This module is for doxygen generation.
+# Defines following macros:
+#   USE_DOXYGEN(doxygen_in doc_dir)
+#   - This macro generate documents according to doxygen template.
+#     Arguments:
+#     + doxygen_in: Doxygen template file.
+#     + doc_dir: Document source directory to be copied from.
+#
+#
 IF(NOT DEFINED _USE_DOXYGEN_CMAKE_)
     SET(_USE_DOXYGEN_CMAKE_ "DEFINED")
 
     MACRO(USE_DOXYGEN doxygen_in doc_dir)
 	FIND_PACKAGE(Doxygen)
 	CONFIGURE_FILE(${doxygen_in} Doxyfile)
+	SET(PACK_SOURCE_IGNORE_FILES ${PACK_SOURCE_IGNORE_FILES} "/Doxyfile$")
 	ADD_CUSTOM_TARGET(doxygen
 	    COMMAND "${DOXYGEN}" "Doxyfile"
 	    )
